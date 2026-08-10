e53b3e3cde1baccf47d1bc7f0ea63cb9009f8ef6
MarkSuttonVsc <mark@visual-software.co.uk>
2026-08-10 07:06:58 +0100

    More updates to admin meny for PCP

--- AdminMenuPlugin/pcc-admin-menu.php
+++ AdminMenuPlugin/pcc-admin-menu.php
@@ -40,9 +40,10 @@ require 'pcc-admin-date-functions.php';
 
 function init_pcc_admin_menu()
 {
-    //add the stylesheet asset
-    wp_register_style( 'pcc-admin-styles', plugins_url('/assets/pcc-admin-styles.css', __FILE__),[], false);
-    wp_register_script( 'pcc-admin-script', plugins_url('/assets/pcc-admin-script.js', __FILE__), [], false);
+    //TO DO add the assets (with dynamic file modification time to prevent caching issues)
+
+    wp_register_style( 'pcc-admin-styles', plugins_url('/assets/pcc-admin-styles.css', __FILE__),[],'1.3.99');
+    wp_register_script( 'pcc-admin-script', plugins_url('/assets/pcc-admin-script.js', __FILE__), [], '1.3.99', array('in_footer' => true));
 
 }
 function enqueue_pcc_admin_styles()
@@ -101,61 +102,67 @@ function pcc_subs_admin_page()
 
     $admin_corrections =  admin_url('admin.php?page=pcc-subs-plan-corrections');
     $admin_quarters =  admin_url('admin.php?page=pcc-subs-quarters');
-    echo esc_html('<h2><img src="'.$pennant.'"/> PCC Subscription Administration</h2><hr/>');   
-    echo esc_html('<p>This is the PCC subscription administration area.</p>');
-    echo esc_html('<p class="alert alert-danger">Please do not operate these functions unless you know what you are doing!</p>'); 
-    echo esc_html('<div class="form-row"><a class="pcc-button indent-100" href="'.$admin_corrections.'">Membership Plan Corrections</a>');
-    echo esc_html('<a class="pcc-button" href="'.$admin_quarters.'">Subscription Quarters Management</a></div>');
+    $content = '<h2><img src="'.$pennant.'"/> PCC Subscription Administration</h2><hr/>';   
+    $content .= '<p>This is the PCC subscription administration area.</p>';
+    $content .= '<p class="alert alert-danger">Please do not operate these functions unless you know what you are doing!</p>'; 
+    $content .= '<div class="form-row"><a class="pcc-button indent-100" href="'.$admin_corrections.'">Membership Plan Corrections</a>';
+    $content .= '<a class="pcc-button" href="'.$admin_quarters.'">Subscription Quarters Management</a></div>';
+    echo wp_kses_post($content);
 }
 
 function pcc_subs_corrections_page()
 {
-    echo esc_html('<h2>PCC Membership Plan Corrections</h2><hr/>'); 
-    echo esc_html('<p>You can use this page to change the subscription plan of a member, when the member has been assigned the wrong plan by mistake.</p>');
-    echo esc_html('<p>Note: this will not change any payment record - you will have to change these using the Paid Member Subscriptions - Payments page.</p>');
-    echo esc_html('<p>Search by first name or last name.</p>');
-    echo esc_html('<div class="form-row"><label for="input_search">Find Member:</label><input id="input_search" placeholder="First or Last Name"/> <button id="button_search" class="pcc-button disabled">Go</button></div>');
-    echo esc_html('<div class="form-row"><label for="list_names">Choose one:</label><select style="width:200px" size="7" id="list_names"></select></div>');
-    echo esc_html('<div class="form-row"><label for="first_name">First Name:</label><input readonly id="first_name"/></div>');
-    echo esc_html('<div class="form-row"><label for="last_name">Last Name:</label><input readonly id="last_name"/></div>');
-    echo esc_html('<div class="form-row"><label for="current_plan">Current Plan:</label><input readonly id="current_plan"/></div>');
-    echo esc_html('<input type="hidden" id="current_plan_id"/>');
-    echo esc_html('<div class="form-row"><label for="list_plans">Change to Plan:</label><select style="width:200px" id="list_plans">');    
-    echo esc_html('</select></div>');
-    echo esc_html('<div class="form-row"><button class="pcc-button indent-100 disabled" id="button_change_plan" >Make Change</button>');
-    echo esc_html('<button class="pcc-button" id="button_reset">Reset</button></div>');
-    echo esc_html('<div class="form-row"><p id="status_bar">Ready...</p></div>');
+    $content = '<h2>Membership Plan Corrections</h2><hr/>';
+    $content .= '<p>This page allows you to correct the membership plan for a member.</p>';
+    $content .= '<p>You can use this page to change the subscription plan of a member, when the member has been assigned the wrong plan by mistake.</p>';
+    $content .= '<p>Note: this will not change any payment record - you will have to change these using the Paid Member Subscriptions - Payments page.</p>';
+    $content .= '<p>Search by first name or last name.</p>';
+    $content .= '<div class="form-row"><label for="input_search">Find Member:</label><input id="input_search" placeholder="First or Last Name"/> <button id="button_search" class="pcc-button disabled">Go</button></div>';
+    $content .= '<div class="form-row"><label for="list_names">Choose one:</label><select style="width:200px" size="7" id="list_names"></select></div>';
+    $content .= '<div class="form-row"><label for="first_name">First Name:</label><input readonly id="first_name"/></div>';
+    $content .= '<div class="form-row"><label for="last_name">Last Name:</label><input readonly id="last_name"/></div>';
+    $content .= '<div class="form-row"><label for="current_plan">Current Plan:</label><input readonly id="current_plan"/></div>';
+    $content .= '<input type="hidden" id="current_plan_id"/>';
+    $content .= '<div class="form-row"><label for="list_plans">Change to Plan:</label><select style="width:200px" id="list_plans">';    
+    $content .= '</select></div>';
+    $content .= '<div class="form-row"><button class="pcc-button indent-100 disabled" id="button_change_plan" >Make Change</button>';
+    $content .= '<button class="pcc-button" id="button_reset">Reset</button></div>';
+    $content .= '<div class="form-row"><p id="status_bar">Ready...</p></div>';
+    echo wp_kses_post($content);
+
     $user = wp_get_current_user() ;
     wp_nonce_field("pcc-subs-correction-ajax-call-nonce-key-".$user->ID, "pcc_subs_correction_nonce_field");
-
+    
+    
 }
 
 function pcc_subs_quarters_page()
 {
-    echo esc_html('<h2>PCC Subscription Quarters Management</h2><hr/>');
-    echo esc_html('<p>The PCC committee has required that subscriptions run from the 1st January every year, for one year.</p>');
-    echo esc_html('<p>This is not how the Paid Member Subscriptions plugin works - so the following administrator interventions are required:</p>');    
-    echo esc_html('<ul class="red-list" style="list-style-type:disc;margin-left:20px;">');
-    echo esc_html("<li>Update discount dates so that the start and finish dates are correct relative to today's date.</li>");
-    echo esc_html('<li>Update the start and finish dates of member subscriptions to align with a calendar year.</li>');    
-    echo esc_html('</ul>');
-    echo esc_html('<div class="form-row"><p id="status_bar">Ready...</p></div>');
-    echo esc_html('<hr/><p></p>');
-    echo esc_html('<h3>PCC Discount Codes</h3>');
-    echo esc_html('<div id="discounts_table">');
-    echo esc_html(get_discounts_table());
-    echo esc_html('</div>');
-    echo esc_html('<div class="form-row"><a id="button_refresh_discounts" class="pcc-button">Refresh Table</a></div>');
+    $content = '<h2>PCC Subscription Quarters Management</h2><hr/>';
+    $content .= '<p>The PCC committee has required that subscriptions run from the 1st January every year, for one year.</p>';
+    $content .= '<p>This is not how the Paid Member Subscriptions plugin works - so the following administrator interventions are required:</p>';    
+    $content .= '<ul class="red-list" style="list-style-type:disc;margin-left:20px;">';
+    $content .= "<li>Update discount dates so that the start and finish dates are correct relative to today's date.</li>";
+    $content .= '<li>Update the start and finish dates of member subscriptions to align with a calendar year.</li>';    
+    $content .= '</ul>';
+    $content .= '<div class="form-row"><p id="status_bar">Ready...</p></div>';
+    $content .= '<hr/><p></p>';
+    $content .= '<h3>PCC Discount Codes</h3>';
+    $content .= '<div id="discounts_table">';
+    $content .= get_discounts_table();
+    $content .= '</div>';
+    $content .= '<div class="form-row"><a id="button_refresh_discounts" class="pcc-button">Refresh Table</a></div>';
     
-    echo esc_html('<p></p><hr/>');
-    echo esc_html('<h3>Subscription Date Alignment</h3>');
-    echo esc_html('<div id="subs_table">');
-    echo esc_html(get_user_subs_table());
-    echo esc_html('</div>');
-    echo esc_html('<div class="form-row"><a id="button_refresh" class="pcc-button">Refresh Table</a></div>');
-
+    $content .= '<p></p><hr/>';
+    $content .= '<h3>Subscription Date Alignment</h3>';
+    $content .= '<div id="subs_table">';
+    $content .= get_user_subs_table();
+    $content .= '</div>';
+    $content .= '<div class="form-row"><a id="button_refresh" class="pcc-button">Refresh Table</a></div>';
+    echo wp_kses_post($content);
     $user = wp_get_current_user() ;
     wp_nonce_field("pcc-quarters-nonce-field-ajax-call-nonce-key-".$user->ID, "pcc_quarters_nonce_field");
     wp_nonce_field("pcc-subs-date-nonce-field-ajax-call-nonce-key-".$user->ID, "pcc_subs_date_nonce_field");
+    
 }
 
